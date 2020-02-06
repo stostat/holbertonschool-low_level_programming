c749b67e2e2e2e49319e312cf2f3912b0ef73b8d
santiago <1494@holbertonschool.com>
2020-02-06 20:26:08 +0000

    if else while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*
+*
+*/
+int main () {
+char ch;
+char m;
+for(ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+for(m = 'A' ; m <= 'Z' ; m++)
+{
+putchar(m);
+}
+return(0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,2 @@
+If else while for
+Usage of loops
