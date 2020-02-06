40320493472d0eb357a79ce4c2365a8f1eb4a0f4
santiago <1494@holbertonschool.com>
2020-02-06 20:54:03 +0000

    changes at size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,15 @@
 */
 int main(void)
 {
-printf("Size of a char: %ld byte(s)\n", sizeof(char));
-printf("Size of an int: %ld byte(s)\n", sizeof(int));
-printf("Size of a long int: %ld byte(s)\n", sizeof(long));
-printf("Size of a long long int: %ld byte(s)\n", sizeof(long long));
-printf("Size of a float: %ld bytes(s)\n", sizeof(float));
+char c;
+int i;
+long l;
+long long int L;
+float f;
+printf("Size of a char: %ld byte(s)\n", sizeof(c));
+printf("Size of an int: %ld byte(s)\n", sizeof(i));
+printf("Size of a long int: %ld byte(s)\n", sizeof(l));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(L));
+printf("Size of a float: %ld bytes(s)\n", sizeof(f));
 return (0);
 }
