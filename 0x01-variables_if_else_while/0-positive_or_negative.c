adb8400503e34c37709caf55c41921ce1bfab579
santiago <1494@holbertonschool.com>
2020-02-06 20:42:27 +0000

    new line added

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,11 +17,11 @@ printf("%i is negative\n", n);
 }
 else if (n == 0)
 {
-printf("%i is zero", n);
+printf("%i is zero\n", n);
 }
 else
 {
-printf("%i is positive", n);
+printf("%i is positive\n", n);
 }
 return (0);
 }
