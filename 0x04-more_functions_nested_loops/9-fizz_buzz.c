ac057bef946c8dcef7eae252a81dcd0a9bdbced3
stostat <santmendieta@icloud.com>
2020-02-13 17:15:46 -0500

    fb

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,14 +14,14 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		printf("FizzBuzz ");
-		else if (i % 3 == 0 && i % 5 != 0)
+		else if (i % 3 == 0)
 		printf("Fizz ");
-		else if (i % 5 == 0 && i % 3 != 0)
+		else if (i % 5 == 0)
 		printf("Buzz ");
 		else if (i == 100)
 		printf("Buzz");
 		else
-		printf("%i ", i);
+		printf("%d ", i);
 	}
 	printf("\n");
 	return (0);
