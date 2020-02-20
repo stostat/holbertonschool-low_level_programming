328403f94cbd74595d67e66e93e1e8166b1ed97b
stostat <santmendieta@icloud.com>
2020-02-19 21:27:38 -0500

    8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_array - prints integers from an array
+ * @a: array
+ * @n: integer
+ * Return: Void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (i < n)
+	{
+		printf("%d", a[i]);
+		if (i != (n - 1))
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+}
