4231a07986ec07ed85ef30dd93108cc0cab09112
stostat <santmendieta@icloud.com>
2020-02-20 15:45:07 -0500

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * _strncmp - check the code for Holberton School students.
+ * _strcmp - check the code for Holberton School students.
  * @s1: char1
  * @s2: char 2
  * Return: int
@@ -11,7 +11,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i=0; *(s1 + i) != '\0'; i++)
+	for (i = 0; *(s1 + i) != '\0'; i++)
 	{
 		if (*(s1 + i) > *(s2 + i))
 			return (*(s1 + i) - *(s2 + i));
