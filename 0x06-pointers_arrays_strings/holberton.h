91a42d761ce77566290edcdbde3a20bb600cbe88
stostat <santmendieta@icloud.com>
2020-02-20 22:41:06 -0500

    putchar added

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,4 +1,13 @@
 #ifndef HOLBERTON_H
+int _putchar(char c);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *rot13(char *);
+char *leet(char *);
+char *cap_string(char *);
+char *string_toupper(char *s);
+void print_array(int *a, int n);
+void reverse_array(int *a, int n);
 int _strcmp(char *s1, char *s2);
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
