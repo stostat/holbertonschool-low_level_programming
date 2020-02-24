6a92f7301eca2faa5f2ea9cc1fe05fef7b16dc72
stostat <santmendieta@icloud.com>
2020-02-24 15:31:22 -0500

    bug fixed

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,20 +11,29 @@ unsigned int _strspn(char *s, char *accept)
 	int i = 0;
 	int j = 0;
 	int k = 0;
+	int br;
 	unsigned int count = 0;
 
 	while (accept[i] != '\0')
 	{
 		i++;
 	}
-	while (s[j] <= s[i])
+
+	while (*(s + j) != '\0')
 	{
+		br = 0;
 		for (k = 0; k <= i; k++)
 		{
 			if (accept[k] == s[j])
+			{
+				br = 1;
 				count++;
+			}
 		}
-		j++;
+		if (br == 1)
+			j++;
+		else
+			break;
 	}
 	return (count);
 }
