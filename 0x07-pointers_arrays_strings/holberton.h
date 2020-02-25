972bb2730257b260759d212694f46ae3a04a8672
stostat <santmendieta@icloud.com>
2020-02-25 14:08:07 -0500

    h

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,5 +1,9 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+int _putchar(char c);
+void print_chessboard(char (*a)[8]);
+char *_strpbrk(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept);
 char *_strchr(char *s, char c);
 char *_memset(char *s, char b, unsigned int n);
 #endif
