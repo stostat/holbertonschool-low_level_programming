8f0c4f64dd48eb923470c2f3885496344428fbf3
stostat <santmendieta@icloud.com>
2020-02-24 22:03:03 -0500

    betty

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,12 @@
 #include "holberton.h"
 #include <stdio.h>
-
+/**
+ * _memset - check the code for Holberton School students.
+ * @s: char
+ * @b: char
+ * @n: int
+ * Return: char.
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,4 +1,13 @@
 #include "holberton.h"
+#include "holberton.h"
+/**
+ * _memcpy - check the code for Holberton School students.
+ * @dest: char
+ * @src: char
+ * @n: int
+ * Return: char.
+ */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
  {
          unsigned int i = 0;
