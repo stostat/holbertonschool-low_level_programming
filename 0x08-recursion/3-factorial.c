3071bbaee057fc36db35237e06568e2dffc1db58
stostat <santmendieta@icloud.com>
2020-02-26 13:58:13 -0500

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * factorial - check the code for Holberton School students.
+ * @n: int
+ * Return: int.
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return -1;
+	if (n == 0)
+		return 0;
+	if (n == 1)
+		return 1;
+	if (n > 1)
+		return (n * factorial(n - 1));
+}
--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,5 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+int factorial(int n);
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
