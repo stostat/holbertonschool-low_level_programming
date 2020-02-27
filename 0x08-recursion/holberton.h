17f8eb66d48c9cd94c3659afa07054337eecbd70
stostat <santmendieta@icloud.com>
2020-02-26 22:04:04 -0500

    h

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -4,5 +4,6 @@ int factorial(int n);
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
+int _sqrt_recursion(int n);
 int _strlen_recursion(char *s);
 #endif
