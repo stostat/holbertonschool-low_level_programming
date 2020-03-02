81ac921d727f7b64615bb0acf3b1d9543f20b6c6
stostat <santmendieta@icloud.com>
2020-03-01 22:25:59 -0500

    change optimized

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,47 +11,35 @@
 
 int main(int argc, char *argv[])
 {
-	int change = 0;
-	int coin = atoi(argv[1]);
+	int change = 0, coin = atoi(argv[1]);
 
-	if (argc != 2)
+	if (argc == 2)
+	{
+		if (coin > 0)
+		{
+			while (coin != 0)
+			{
+				if (coin >= 25)
+					coin -= 25;
+				else if (coin >= 10 && coin < 25)
+					coin -= 10;
+				else if (coin >= 5 && coin < 10)
+					coin -= 5;
+				else if (coin >= 2 && coin < 5)
+					coin -= 2;
+				else if (coin == 1)
+					coin -= 1;
+				change++;
+			}
+			printf("%d\n", change);
+		}
+		else
+		printf("0\n");
+		return (0);
+	}
+	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-	if (coin >= 0)
-	{
-		while (coin != 0)
-		{
-			if (coin >= 25)
-			{
-				coin -= 25;
-				change++;
-			}
-			if (coin >= 10 && coin < 25)
-			{
-				coin -= 10;
-				change++;
-			}
-			if (coin >= 5 && coin < 10)
-			{
-				coin -= 5;
-				change++;
-			}
-			if (coin >= 2 && coin < 5)
-			{
-				coin -= 2;
-				change++;
-			}
-			if (coin == 1)
-			{
-				coin -= 1;
-				change++;
-			}
-		}
-		printf("%d\n", change);
-	}
-	else
-		printf("0\n");
-	return (0);
 }
