83d42445a2fa0c9766dd73b2c720a9c5c3d4c948
stostat <santmendieta@icloud.com>
2020-03-01 22:51:54 -0500

    segmentation fault corrected

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,35 +11,29 @@
 
 int main(int argc, char *argv[])
 {
-	int change = 0, coin = atoi(argv[1]);
+	int change = 0, coin;
 
-	if (argc == 2)
-	{
-		if (coin > 0)
-		{
-			while (coin != 0)
-			{
-				if (coin >= 25)
-					coin -= 25;
-				else if (coin >= 10 && coin < 25)
-					coin -= 10;
-				else if (coin >= 5 && coin < 10)
-					coin -= 5;
-				else if (coin >= 2 && coin < 5)
-					coin -= 2;
-				else if (coin == 1)
-					coin -= 1;
-				change++;
-			}
-			printf("%d\n", change);
-		}
-		else
-		printf("0\n");
-		return (0);
-	}
-	else
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	coin = atoi(argv[1]);
+
+	while (coin > 0)
+	{
+		if (coin >= 25)
+			coin -= 25;
+		else if (coin >= 10 && coin < 25)
+			coin -= 10;
+		else if (coin >= 5 && coin < 10)
+			coin -= 5;
+		else if (coin >= 2 && coin < 5)
+			coin -= 2;
+		else if (coin == 1)
+			coin -= 1;
+		change++;
+	}
+	printf("%d\n", change);
+	return (0);
 }
