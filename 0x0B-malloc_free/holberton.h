1ca09bc775ebdbef682da2ec859635d2b2d1e518
stostat <santmendieta@icloud.com>
2020-03-03 14:37:46 -0500

    holb 2

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -1,7 +1,9 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 int _putchar(char c);
+int **alloc_grid(int width, int height);
 char *create_array(unsigned int size, char c);
 void simple_print_buffer(char *buffer, unsigned int size);
-
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 #endif
