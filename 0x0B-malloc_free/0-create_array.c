b212e9ed2b1cee5a483b466e4452ee8595f1462f
stostat <santmendieta@icloud.com>
2020-03-03 12:03:53 -0500

    malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * create_array - prints buffer in hexa
+ * @size: int
+ * @c: char
+ *
+ * Return: Char
+ */
+char *create_array(unsigned int size, char c)
+{
+	int i;
+	char *p;
+
+	p = malloc(size * sizeof(c));
+	for (i = 0; i < size; i++)
+	{
+		p[i] = c;
+	}
+	return (p);
+}
+
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,3 @@
+Malloc
+
+Memory allocation
