a8614c61d8a66d34a367c510263d1359928e61c0
stostat <60362885+stostat@users.noreply.github.com>
2020-03-04 13:19:11 -0500

    Update 4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,9 +2,7 @@
 /**
  * free_grid - prints a grid of integers
  * @grid: the address of the two dimensional grid
- * @height: height of the grid
- *
- * Return: Nothing.
+ * @height: height int
  */
 
 void free_grid(int **grid, int height)
