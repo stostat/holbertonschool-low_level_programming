272095df145034be2a669fdae7ecef693c3bd7fa
stostat <santmendieta@icloud.com>
2020-03-09 14:16:58 -0400

    holberton corrected

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -15,4 +15,5 @@ void *malloc_checked(unsigned int b);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _isdig(char *num);
+char *_memcpy(char *dest, char *src, unsigned int n);
 #endif
