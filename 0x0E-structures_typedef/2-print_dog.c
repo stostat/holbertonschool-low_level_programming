6267678d9d3c8cdfe3874de6ab5fc446fad131a0
stostat <santmendieta@icloud.com>
2020-03-10 18:17:05 -0500

    2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,2 +1,23 @@
 #include "dog.h"
 #include <stdio.h>
+
+/**
+ * print_dog - prints dog info
+ * @d: char
+ * Return: Void.
+ */
+
+void print_dog(struct dog *d)
+{
+	if (!d)
+		return;
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	if (d->owner == NULL)
+		printf("Owner: (nil)");
+	else
+		printf("Owner: %s\n", d->owner);
+}
