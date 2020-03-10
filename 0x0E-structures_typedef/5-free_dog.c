c4565e5a5f7122795ed60418aef3333a808db3cf
stostat <santmendieta@icloud.com>
2020-03-10 18:49:45 -0500

    5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,2 +1,15 @@
-#include "dog.h"
 #include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - lets the dogs out.
+ * @d: pointer.
+ */
+void free_dog(dog_t *d)
+{
+	if (!d)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
