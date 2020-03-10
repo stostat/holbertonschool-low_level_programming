6ccf2aa8f04b1deb261c1e2c977ceea16ef4528b
stostat <santmendieta@icloud.com>
2020-03-10 18:43:09 -0500

    4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,2 +1,50 @@
 #include "dog.h"
 #include <stdio.h>
+/**
+  * new_dog - creates new dog
+  * @name: char
+  * @age: float
+  * @owner: char
+  * Return: struct
+  */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ptr;
+	char *mallname, *mallowner;
+	int i, j, k, l;
+
+	for (i = 0; name[i]; i++)
+		;
+	for (j = 0; owner[j]; j++)
+		;
+	mallname = malloc(i + 1);
+	if (malname == NULL)
+		return (NULL);
+
+	mallowner = malloc(j + 1);
+	if (mallowner == NULL)
+	{
+		free(mallname);
+		return (NULL);
+	}
+
+	for (k = 0; k <= i; k++)
+		mallname[k] = name[k];
+
+	for (l = 0; l <= j; l++)
+		mallowner[l] = owner[l];
+
+	ptr = malloc(sizeof(dog_t));
+	if (ptr == NULL)
+	{
+		free(mallname);
+		free(mallowner);
+		return (NULL);
+	}
+
+	ptr->name = mallname;
+	ptr->age = age;
+	ptr->owner = mallowner;
+	return (ptr);
+}
