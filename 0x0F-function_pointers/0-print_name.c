961c02d38238e1600c1b089c5323dadac28ec421
stostat <santmendieta@icloud.com>
2020-03-11 18:59:40 -0500

    name correction

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - function that prints a name
+ * @name: pointer to a char
+ * @f: pointer to a function
+ * Return: void
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
