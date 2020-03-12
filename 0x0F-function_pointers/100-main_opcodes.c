cf7b29d7e3895812a90310fe62dc8a09b356e1c8
stostat <santmendieta@icloud.com>
2020-03-12 17:30:27 -0500

    advanced corrected

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -8,17 +8,29 @@
   * Return: int
   */
 
-int main(int argc, int *argv[])
+int main(int argc, char **argv)
 {
+	char *ptr =(char *) main;
+	int i, j;
+
+
 	if (argc != 2)
 	{
-		printf("Error\n");
-		exit(1)
+i = atoi(argv[1]);		printf("Error\n");
+		exit(1);
 	}
-	if (atoi(argv[1] < 0))
+	i = atoi(argv[1]);
+
+	if (i < 0)
 	{
 	printf("Error\n");
 	exit(2);
 	}
+	else
+	{
+		for (j = 0; j < i; j++)
+			printf("%02hhx ", ptr[j]);
+	}
+	printf("\n");
 	return (0);
 }
