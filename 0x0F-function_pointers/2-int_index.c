9fe6d2d5f96e41d0974867c53c9b9bd2052c110d
stostat <santmendieta@icloud.com>
2020-03-11 21:19:53 -0500

    2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 #include "function_pointers.h"
 /**
- * main - check the code for Holberton School students.
- *
+ * int_index - check the code for Holberton School students.
+ * @array: int
+ * @size: int
+ * @cmp: int
  * Return: Always 0.
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, j;
+
 	if (size > 0)
 	{
-	for (i = 0; i < size; i++)
-	{
-	j = cmp(array[i]);
-	if (j == 0)
-	return (i);
-	}
+		for (i = 0; i < size; i++)
+		{
+			j = cmp(array[i]);
+			if (j == 0)
+				return (i);
+		}
 	}
 	return (-1);
 }
