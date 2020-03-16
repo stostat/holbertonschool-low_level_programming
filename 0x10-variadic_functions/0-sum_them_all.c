ecc7b4f7a7c7684454244130ccbac9a2a5b2bfc5
stostat <santmendieta@icloud.com>
2020-03-15 21:26:36 -0500

    hot fix

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,16 +11,17 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list num;
-	int sum;
+	int sum = 0;
 	unsigned int i;
 
 	if (n == 0)
 		return (0);
-	sum = 0;
 	va_start(num, n);
 
 	for (i = 0; i < n; i++)
+	{
 		sum += va_arg(num, int);
+	}
 	va_end(num);
 
 	return (sum);
