867734250128f4d5517464c4e2eea65dbd5771d2
stostat <santmendieta@icloud.com>
2020-03-15 21:34:34 -0500

    permissions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -17,12 +17,8 @@ int sum_them_all(const unsigned int n, ...)
 	if (n == 0)
 		return (0);
 	va_start(num, n);
-
 	for (i = 0; i < n; i++)
-	{
 		sum += va_arg(num, int);
-	}
 	va_end(num);
-
 	return (sum);
 }
