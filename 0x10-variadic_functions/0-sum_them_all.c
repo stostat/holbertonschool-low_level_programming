9c97a5ce88127723b8a7525d6c0b908e642e41d5
stostat <santmendieta@icloud.com>
2020-03-15 21:43:19 -0500

    hot fix

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
 #include "variadic_functions.h"
-#include <stdarg.h>
 
 /**
  * sum_them_all - function that returns the sum of all its parameters
@@ -10,9 +8,9 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list num;
 	int sum = 0;
 	unsigned int i;
+	va_list num;
 
 	if (n == 0)
 		return (0);
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,16 @@
 #ifndef _VARIADIC_FUNCTIONS_H_
 #define _VARIADIC_FUNCTIONS_H_
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+/**
+  * struct ops - checks operator
+  * @op: operator
+  * @f: sunction
+  */
+
 typedef struct ops
 {
     char *op;
