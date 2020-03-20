9bcf1c4207775a7a970b2fc8076be50a7bb8b213
stostat <santmendieta@icloud.com>
2020-03-19 23:05:15 -0500

    [FIX] zero

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+
 /**
  * print_list - returns the number of elements in a linked list_t list.
  * @h: pointer
@@ -10,16 +11,16 @@
 
 size_t print_list(const list_t *h)
 {
-	int count = 0;
+	unsigned int count = 0;
 
 	while (h)
 	{
-		if (h->str == NULL)
+		if (!h->str)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
 		count++;
+		h = h->next;
 	}
 	return (count);
 }
