915823c9c43ff96bbe239fb8ed61a46ea244ce3b
stostat <santmendieta@icloud.com>
2020-03-24 18:49:08 -0500

    [FIX] 0 correction on return value

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,10 +11,11 @@
 
 size_t print_listint(const listint_t *h)
 {
+	size_t i = 0;
 	while (h)
 	{
-		printf("%d\n", h->n);
+		printf("%i\n", h->n);
 		h = h->next;
 	}
-	return (0);
+	return (i);
 }
