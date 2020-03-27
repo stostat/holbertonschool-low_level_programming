66bf0c7a172ec0c8071f0761b8affc3b79d20a63
stostat <60362885+stostat@users.noreply.github.com>
2020-03-26 23:58:16 -0500

    Update 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -5,10 +5,10 @@
  * @index: int
  * Return: int
  */
-int clear_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > (sizeof(n) * 8))
 		return (-1);
-	*n &= ~(1 << index);
+	*n &= (~(1 << index));
 	return (1);
 }
