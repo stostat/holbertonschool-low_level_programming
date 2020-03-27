d082fd02728c280948003acfbb90b05099d0fb1c
stostat <santmendieta@icloud.com>
2020-03-26 23:40:01 -0500

    [FIX] second file

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,15 +1,17 @@
 #include "holberton.h"
+
 /**
  * get_bit - returns value of bit at ith index
  * @n: long int
  * @index: int
  * Return: int
  */
+
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index > (sizeof(n) * 8))
+	if (index >= (sizeof(n) * 8))
 		return (-1);
-	int bit = (n & (1 << index)) >> index;
+	int bit = (n >> index) & 1;
 
 	return (bit);
 }
