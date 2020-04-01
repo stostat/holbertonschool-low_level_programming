a9c774b59f5a117cb57b5984ed443644e9ffa775
stostat <santmendieta@icloud.com>
2020-03-31 21:54:48 -0500

    [FEATURE] second exercise added

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,14 +1,27 @@
 #include "holberton.h"
+/**
+  * append_text_to_file - appends at the end of text
+  * @filename: char
+  * @text_content: text to be appended
+  * Return: int
+  */
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, ap;
-	mode_t mode = 
+	int fd, ap, i;
 
 	if (!filename)
 		return (-1);
-	if (text_content == NULL)
-		return (1);
-	fd = open(filename, O_RDWR | O_APPEND, )
+	if (!text_content)
+	return (1);
+	fd = open(filename, O_RDWR | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	for (i = 1; text_content[i]; i++)
+		;
+	ap = write(fd, text_comtent, i);
+	close(fd);
+	if (ap == -1)
+		return (-1);
 }
 
