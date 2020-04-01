77a14ce6404c6c8d0c512fc9fa78e0137c321c6c
stostat <santmendieta@icloud.com>
2020-03-31 22:48:31 -0500

    FIX 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,9 +20,9 @@ int create_file(const char *filename, char *text_content)
 	while (text_content[i])
 		i++;
 	w = write(fd, text_content, i);
+	close(fd);
 	if (w == -1)
 		return (-1);
-	close(fd);
 	return (1);
 }
 
