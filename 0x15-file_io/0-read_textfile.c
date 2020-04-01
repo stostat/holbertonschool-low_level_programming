7d9760bd950d8abca8d373123ca9c817e536092b
stostat <santmendieta@icloud.com>
2020-03-31 22:57:54 -0500

    [FIX] in 0 exercise char lenght not allowed

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,16 +11,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t fdr;
-	char buf[letters + 1];
+	char *buf;
 
 	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
+	buf = malloc(letters * sizeof(char));
 	fdr = read(fd, buf, letters);
 	buf[letters] = '\0';
 	write(STDOUT_FILENO, buf, fdr);
 	close(fd);
+	free(buf);
 	return (fdr);
 }
