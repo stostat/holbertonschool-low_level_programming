1865c20c4fbb17765d6ade6ac187a13bda8d02ac
stostat <santmendieta@icloud.com>
2020-05-02 16:29:33 -0500

    first commit

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,22 +10,22 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    const dlistint_t *temp = NULL;
-    size_t i = 0;
+	const dlistint_t *temp = NULL;
+	size_t i = 0;
 
-    if(!h)
-        return (0);
-    temp = h;
-    while (temp->prev)
-    {
-        temp = temp->prev;
-        i++;
-    }
-    while (temp)
-    {
-        printf("%d\n", temp->n);
-        temp = temp->next;
-        i++;
-    }
-    return (i);
+	if (!h)
+	return (0);
+	temp = h;
+	while (temp->prev)
+	{
+		temp = temp->prev;
+		i++;
+	}
+	while (temp)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+		i++;
+	}
+	return (i);
 }
