40efa8e657607ac8d4d6b45887ac21fbd19c3bfc
stostat <santmendieta@icloud.com>
2020-05-02 16:37:19 -0500

    1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - function that prints a doubly linked list
+ * @h: pointer to the header
+ * Return: integer
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *temp = NULL;
+	size_t i = 0;
+
+	if (!h)
+	return (0);
+	temp = h;
+	while (temp->prev)
+	{
+		temp = temp->prev;
+		i++;
+	}
+	while (temp)
+	{
+		temp = temp->next;
+		i++;
+	}
+	return (i);
+}
