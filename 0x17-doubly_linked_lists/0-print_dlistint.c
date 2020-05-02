d236962986f474632da92daa841d2b2e385d6a65
stostat <santmendieta@icloud.com>
2020-05-02 16:17:46 -0500

    first commit

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - function that prints a doubly linked list
+ * @h: pointer to the header
+ * Return: integer
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+    const dlistint_t *temp = NULL;
+    size_t i = 0;
+
+    if(!h)
+        return (0);
+    temp = h;
+    while (temp->prev)
+    {
+        temp = temp->prev;
+        i++;
+    }
+    while (temp)
+    {
+        printf("%d\n", temp->n);
+        temp = temp->next;
+        i++;
+    }
+    return (i);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,4 @@
+DOUBLY LINKED LISTS
+
+In this peoject we will learn about doubly linked lists, which are lists whose nodes can
+point to either direction, and the head and the last node will be pointing to null
