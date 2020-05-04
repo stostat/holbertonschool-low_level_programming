5f1445dddd76b45fa90ec1cc75d147b5f889bf29
stostat <santmendieta@icloud.com>
2020-05-03 23:59:44 -0500

    psw2

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - returns a node at a given index
+ * @h:  pointer to the head of the list
+ * @idx: index number
+ * @n: data in new node
+ * Return: node
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	
+}
