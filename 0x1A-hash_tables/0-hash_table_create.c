689bdfde365447b14ac2490308dff0cec14a9b01
stostat <santmendieta@icloud.com>
2020-05-29 16:33:18 -0500

    betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,18 +3,20 @@
 /**
  * hash_table_create - Creates a hash table
  * @size: size of the table
- * @return: Pointer to hash table
+ * Return: Pointer to hash table
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht = malloc(sizeof(hash_table_t) * 1);
-	ht->array = malloc(sizeof(hash_node_t*) * size);
+
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+
 	unsigned int i = 0;
 
-	for(; i < size; i++)
+	for (; i < size; i++)
 	{
 		ht->array[i] = NULL;
 	}
-	return(ht);
+	return (ht);
 }
