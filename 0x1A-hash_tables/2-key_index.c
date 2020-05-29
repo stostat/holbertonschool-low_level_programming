12e6bc924feba2788fd4a27584cfd9d9d135e19d
stostat <santmendieta@icloud.com>
2020-05-29 18:01:46 -0500

    refactor

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,9 +1,9 @@
 #include "hash_tables.h"
 
 /**
- * key_index - find a key anr returns index 
+ * key_index - find a key anr returns index
  * @key: key char to be compared
- * @ size: size of table
+ * @size: size of table
  * Return: index of key or 0 if not found
  */
 
