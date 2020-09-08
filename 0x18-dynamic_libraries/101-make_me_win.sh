7675582a8c2cf4cfbb44abb988e2f2a7bbeb49e3
stostat <santmendieta@icloud.com>
2020-09-07 22:44:05 -0500

    script

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,4 @@
 
 #!/bin/bash
-wget -P 
-export LD_PRELOAD=/tmp/jackpoint.so
+wget -P "https://github.com/stostat/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/bingo.so"
+export LD_PRELOAD=/tmp/bingo.so
